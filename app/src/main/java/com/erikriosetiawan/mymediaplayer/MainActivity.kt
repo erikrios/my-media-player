ba63c7f2c106b53229bf26bd8718000bc0466ec9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-11 20:09:32 +0700

    Implements the media player

--- app/src/main/java/com/erikriosetiawan/mymediaplayer/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mymediaplayer/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.erikriosetiawan.mymediaplayer
 
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.mymediaplayer.databinding.ActivityMainBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,5 +20,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        mMediaPlayer = MediaPlayer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val attribute = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+            mMediaPlayer?.setAudioAttributes(attribute)
+        } else {
+            mMediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        }
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
+        try {
+            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        mMediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mMediaPlayer?.start()
+        }
+        mMediaPlayer?.setOnErrorListener { mp, what, extra -> false }
     }
 }
